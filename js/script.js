9938e6ac21dd8c3f58d3ad9efa5a6af9999ef356
at143 <anshulkumar@yahoo.com>
2024-03-20 16:56:34 -0400

    fixed empForm spacing

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let empForm
 // HELPER FUNCTION TO RETURN DOM ELEMENT
 const $ = (id) => document.getElementById(id)
 
-empForm       = $('empForm')
+empForm = $('empForm')
 
 empForm.addEventListener("submit", (e) => {
     e.preventDefault();
