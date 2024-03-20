e5134f0cb01aa0e4ffd9a8912a789679023faf02
at143 <anshulkumar@yahoo.com>
2024-03-20 16:55:37 -0400

    reduced js to 15 lines

--- js/script.js
+++ js/script.js
@@ -1,29 +1,15 @@
-// GLOBAL VARIABLES
-let id
-let employeeName
-let ext
-let email
-let department
-let submit
 let empForm
 
 // HELPER FUNCTION TO RETURN DOM ELEMENT
 const $ = (id) => document.getElementById(id)
 
-// GET THE DOM ELEMENTS
-id            = $('id')
-employeeName  = $('name')
-ext           = $('ext')
-email         = $('email')
-department    = $('department')
 empForm       = $('empForm')
 
 empForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    console.log(`ID: ${id.value}`)
-    console.log(`Name: ${employeeName.value}`)
-    console.log(`Extension: ${ext.value}`)
-    console.log(`Email: ${email.value}`)
-    console.log(`Department: ${department.value}`)
+    console.log(`ID: ${$('id').value}`)
+    console.log(`Name: ${$('name').value}`)
+    console.log(`Extension: ${$('ext').value}`)
+    console.log(`Email: ${$('email').value}`)
+    console.log(`Department: ${$('department').value}`)
   });
